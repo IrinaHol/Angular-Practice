150509989e34c0e5a808b01f415f5b9c668cd833
Ira Holovatska <72762647+IrinaHol@users.noreply.github.com>
2021-01-21 17:56:48 +0200

    Practice_ng1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersWithAddressComponent } from './users-with-address/users-with-address.component';
+import { UserComponent } from './user/user.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersWithAddressComponent,
+    UserComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule
