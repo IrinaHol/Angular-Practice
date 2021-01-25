f01c2034994f97014e46109e42507ab9bd865a0b
Ira Holovatska <72762647+IrinaHol@users.noreply.github.com>
2021-01-25 19:03:28 +0200

    practice3_angular

---
+++ src/app/data/data.ts
@@ -0,0 +1,7 @@
+export const users = [
+  {id: 1, name: 'Ira', age: 25, status: true},
+  {id: 2, name: 'Ira', age: 25, status: false},
+  {id: 3, name: 'Ira', age: 25, status: true},
+  {id: 4, name: 'Ira', age: 25, status: false},
+  ];
+
